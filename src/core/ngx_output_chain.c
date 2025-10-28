9fb183329a45b82ef74018b3ec7a46ed9c61265d
Tiffany Citra <tiffanycitra95@gmail.com>
2025-10-28 16:44:19 -0700

    Remove unsafe reverse range handling

--- src/core/ngx_output_chain.c
+++ src/core/ngx_output_chain.c
@@ -624,7 +624,6 @@ ngx_output_chain_copy_buf(ngx_output_chain_ctx_t *ctx)
             return NGX_ERROR;
         }
 
-        dst->rev = src->rev;
         dst->last += n;
 
         if (sendfile) {
