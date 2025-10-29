ac26e8209b6797f905115a9bd04247bb77cf4194
Tiffany Citra <tiffanycitra95@gmail.com>
2025-10-28 17:07:45 -0700

    Remove unsafe reverse range handling

--- src/core/ngx_buf.h
+++ src/core/ngx_buf.h
@@ -25,7 +25,6 @@ struct ngx_buf_s {
 
     u_char          *start;         /* start of buffer */
     u_char          *end;           /* end of buffer */
-    ngx_int_t        rev;
     ngx_buf_tag_t    tag;
     ngx_file_t      *file;
     ngx_buf_t       *shadow;
