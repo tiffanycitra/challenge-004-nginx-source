455669b10f70000de1312ea80d61ab7a61cf5056
Tiffany Citra <tiffanycitra95@gmail.com>
2025-10-28 18:48:54 -0700

    Fix POP3 login buffer overflow

--- src/mail/ngx_mail_pop3_handler.c
+++ src/mail/ngx_mail_pop3_handler.c
@@ -329,7 +329,7 @@ ngx_mail_pop3_user(ngx_mail_session_t *s, ngx_connection_t *c)
 
     arg = s->args.elts;
     s->login.len = arg[0].len;
-    s->login.data = ngx_pnalloc(c->pool, 100);
+    s->login.data = ngx_pnalloc(c->pool, s->login.len);
     if (s->login.data == NULL) {
         return NGX_ERROR;
     }
